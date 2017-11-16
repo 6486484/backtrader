a479fe928c224307599a8c0df82533970f8cba71
Rich O'Regan <31622951+rich-oregan@users.noreply.github.com>
2017-11-16 22:42:03 +0530

    Progress..

--- backtrader/analyzers/traderecorder.py
+++ backtrader/analyzers/traderecorder.py
@@ -40,21 +40,25 @@ class TradeRecorder(Analyzer):
 
 
     def create_analysis(self):
-
+        #print('CUNT TRADE RECORDER')
         # Keep dict of trades based on tradeid
         # Note: must be a unique tradeid defined for each trade else will
         # get overwritten..
         self.rets = AutoOrderedDict()
         self.rets.openTrades = []   # List of all open trades..
         self.rets.closedTrades = [] # List of all closed trades..
-        self._tradeDict = {}        # Hidden from user, useful to track trades..
+
+        # Hidden from user, destroyed at end, useful to track trades..
+        self._tradeDict = {}    # Temp store Trade objects..
 
 
     def notify_trade(self, trade):
 
+        # Add trade to our trade dict which records all trades..
         # Great thing is, don't care if open or closed, if an open trade
         # becomes closed, it is simply rewritten.
         # Note: must be a unique tradeid for each trade else overwritten.
+        #print('type trade = ',type(trade))
         self._tradeDict[trade.tradeid]=trade
 
 
@@ -63,11 +67,48 @@ class TradeRecorder(Analyzer):
 
         # Create our output list of closed and open trades we have tracked..
         for n in self._tradeDict:
-            t = self._tradeDict[n]   # Get value (ie Trade object)
-            if t.isopen:
-                self.rets.openTrades.append(t)
+
+            trade = self._tradeDict[n]   # Get value (ie Trade object)
+
+            # Create dictionary object of essential attributes of Trade object..
+            # Note: we dont save Trade objects because they are inefficient and
+            # each Trade saves market data (retarded), also when I split data into
+            # three, open only, normal, close only. Created problems with optimiser
+            # pickling..
+
+            if trade.isopen:
+                # Trade open..
+                _trade={}
+                _trade['entry_price']=trade.entry_price
+                _trade['entry_date']=trade.open_datetime()
+                _trade['pnl']=trade.pnl
+                _trade['pnlcomm']=trade.pnlcomm
+                _trade['tradeid']=trade.tradeid
+                _trade['islong']=trade.long
+                if hasattr(trade,'R'):
+                    _trade['R']=trade.R    # Apend R stop if it exists..
+                self.rets.openTrades.append(_trade)  # Save open trade dict
+
             else:
-                self.rets.closedTrades.append(t)
+                # Trade closed, add attributes above + exit price & date..
+                _trade={}
+                _trade['entry_price']=trade.entry_price
+                _trade['entry_date']=trade.open_datetime()
+                _trade['exit_price']=trade.exit_price
+                _trade['exit_date']=trade.close_datetime()
+                _trade['pnl']=trade.pnl
+                _trade['pnlcomm']=trade.pnlcomm
+                _trade['tradeid']=trade.tradeid
+                _trade['islong']=trade.long
+                if hasattr(trade,'R'):
+                    _trade['R']=trade.R    # Apend R stop if it exists..
+                self.rets.closedTrades.append(_trade)  # Save closed trade dict
+
+        # Now kill the internal trade dict of Trade objects..
+        # Highly inefficient, we don't want it to be saved with the
+        # BackTrader Cerebro optimisation feature..
+        self._tradeDict = None
+
         self.rets._close()    # Check if we need this..
 
 
